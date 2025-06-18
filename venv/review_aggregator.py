c3066af0acdf357bd294fac4c76001cf3b4550e7
zkralec <zkralec@icloud.com>
2025-06-17 22:51:26 -0400

    Added average website review based on scraped information

--- venv/review_aggregator.py
+++ venv/review_aggregator.py
@@ -2,7 +2,21 @@ import requests
 import csv
 
 from bs4 import BeautifulSoup
+
 from urllib.parse import urljoin
+
+# Method for calculating average review
+def avg_review(rating_list):
+
+    sum = 0
+
+    if not rating_list:
+        print("\nNo ratings to average.\n")
+        return
+    else:
+        for i in rating_list:
+            sum += i
+        return round(sum / len(rating_list), 2)
 
 # Prompt user for the company they want to review
 web_name = input('\n\nPlease enter weblink of the company you want reviews for.\nNote, quick back to back requests can cause issues. \n\nEx. amazon.com \n    nike.com \n    temu.com \n\nYour selection: ')
@@ -11,6 +25,7 @@ web_name = input('\n\nPlease enter weblink of the company you want reviews for.\
 url = 'https://www.trustpilot.com/review/' + web_name
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
 
+# Ensure url works
 try:
     response = requests.get(url, headers=headers, timeout=10)
     if response.status_code == 404:
@@ -27,12 +42,20 @@ except requests.exceptions.RequestException as e:
     exit()
 
 # Prompt user for number of reviews they want
-max_reviews = input("\nHow many reviews would you like to recieve?\n\n")
+max_reviews = input("\nHow many reviews would you like to scrape?\n\n")
 
+# Limit user options to be reasonable
 try:
-    max_reviews = int(max_reviews)
+    if int(max_reviews) >= 50000:
+        print('\n❌ Please enter a valid number less than 50000.\n')
+        exit()
+    elif int(max_reviews) > 0:
+        max_reviews = int(max_reviews)
+    else:
+        print('\n❌ Please enter a valid number greater than 0.\n')
+        exit()
 except ValueError:
-    print("\n❌ Please enter a valid whole number.\n")
+    print('\n❌ Please enter a valid number greater than 0.\n')
     exit()
 
 # Parsing the HTML
@@ -49,6 +72,9 @@ href = 'temp'
 # Initializing counter for reviews scraped
 num_reviews = 0
 
+# Initializing list for all ratings
+rating_list = []
+
 while href is not None:
 
     # Get review block
@@ -60,15 +86,21 @@ while href is not None:
         # End program if number of reviews satisfies user selection
         if num_reviews >= max_reviews:
             review_file.close()
-            print('✅ DONE: Check the "reviews.csv" file for all scraped reviews.')
+            print(f'\nThe average rating for {web_name} was {avg_review(rating_list)}/5.\n')
+            print('✅ DONE: Check the "reviews.csv" file for all scraped reviews.\n')
             exit()
 
+        # Getting desired text for author, rating, and review from the block
         author = i.find('span', class_='typography_heading-xs__osRhC typography_appearance-default__t8iAq').get_text()
         rating_tag = i.find('div', class_='star-rating_starRating__sdbkn star-rating_medium__Oj7C9').find('img')
         rating = rating_tag.get('alt') if rating_tag else 'N/A'
         review_tag = i.find('p', class_='typography_body-l__v5JLj typography_appearance-default__t8iAq')
         review = review_tag.get_text() if review_tag else 'N/A'
 
+        # Adding each rating to the list
+        rating_list.append(float(rating[6]))
+
+        # Write to file and increase reviews found
         writer.writerow([author, rating, review])
         num_reviews += 1
 
@@ -82,12 +114,12 @@ while href is not None:
     # Gets the new link and page
     if href and href.get('href'):
         next_page_url = urljoin(url, href['href'])
-        print('Scraping: ' + next_page_url)
         response = requests.get(next_page_url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
     else:
         href = None
 
-# Closing the CSV file
+# Closing the CSV file and final print statements
 review_file.close()
-print('⚠️ DONE: There are less reviews than what you requested. Scraped all reviews instead. Check the "reviews.csv" file for all scraped reviews')
+print(f'\nThe average rating for {web_name} was {avg_review(rating_list)}/5.\n')
+print('⚠️ DONE: There are less reviews than what you requested. Scraped all reviews instead. Check the "reviews.csv" file for all scraped reviews.\n')
